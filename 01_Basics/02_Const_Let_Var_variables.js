160af601bb294b344864228ea529d386fca51048
Bhavesh7755 <161071443+Bhavesh7755@users.noreply.github.com>
2024-02-29 18:00:09 +0000

    variable

---
+++ 01_Basics/02_Const_Let_Var_variables.js
@@ -0,0 +1,9 @@
+const accountId = 7755573
+let accountEmail = "bhavesh123@google.com"
+var password = "5756475736"
+accountCity = "kundanpur"
+let accountState;
+//accountId = 45737583   it is not allowed, because accountId is constant
+
+console.log(accountId);
+console.table([accountId,accountEmail,password,accountCity,accountState]);
