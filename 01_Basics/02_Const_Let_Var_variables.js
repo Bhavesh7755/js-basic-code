ff54527b560d7719258bb563b7519bf4ff55fb3d
Bhavesh7755 <161071443+Bhavesh7755@users.noreply.github.com>
2024-03-04 17:44:19 +0000

    conversion of datatype

--- 01_Basics/02_Const_Let_Var_variables.js
+++ 01_Basics/02_Const_Let_Var_variables.js
@@ -3,7 +3,7 @@ let accountEmail = "bhavesh123@google.com"
 var password = "5756475736"
 accountCity = "kundanpur"
 let accountState;
-//accountId = 45737583   it is not allowed, because accountId is constant
+//accountId = 45737583  it is not allowed, because accountId is constant
 
 console.log(accountId);
 console.table([accountId,accountEmail,password,accountCity,accountState]);
---
+++ 01_Basics/03_Conversion.js
@@ -0,0 +1,39 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "Bhavesh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); // output will be NaN(Not a number)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = 69
+// let isLoggedIn = "bhavesh"
+// let isLoggedIn = ""
+
+// let numberInBoolean = Boolean(isLoggedIn)
+
+// console.log(typeof numberInBoolean);
+// console.log(numberInBoolean)
+
+let someNumber = 45
+
+let numberInString = String(someNumber)
+
+console.log(typeof numberInString);
+console.log(numberInString);
