98822213c888f01bc770187d74fa891e3a28502e
Bhavesh7755 <161071443+Bhavesh7755@users.noreply.github.com>
2024-03-05 16:31:15 +0000

    String to number and other operation

---
+++ 01_Basics/04_String_To_Number.js
@@ -0,0 +1,21 @@
+// let str1 = "Bhavesh";
+// let str2 = " Parmar";
+
+// let str3 = str1+str2;
+
+// console.log(str3);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(true);
+console.log(+true);
+// console.log(true+); it is invalid
+console.log(+"");
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
